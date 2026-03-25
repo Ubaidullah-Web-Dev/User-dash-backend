4158ac2a2f2735a3f217092b38afbdf1dd056318
Ubaid <ubaidullah.web.dev@gmail.com>
2026-03-25 19:09:07 +0500

    first commit

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -4,6 +4,7 @@ namespace App\EventListener;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 use Symfony\Component\HttpFoundation\RequestStack;
+use App\Entity\User;
 
 class JWTCreatedListener
 {
@@ -16,12 +17,20 @@ class JWTCreatedListener
 
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
-        $request = $this->requestStack->getCurrentRequest();
-        if (!$request) {
+        $user = $event->getUser();
+        if (!$user instanceof User) {
             return;
         }
 
+        $request = $this->requestStack->getCurrentRequest();
         $payload = $event->getData();
+        $payload['name'] = $user->getName();
+
+        if (!$request) {
+            $event->setData($payload);
+            return;
+        }
+
         $content = json_decode($request->getContent(), true);
 
         if (isset($content['remember_me']) && $content['remember_me'] === true) {
